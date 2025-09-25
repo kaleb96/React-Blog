45406eddf5830255771862c07ee53459550fc536
yes505304@ipactory.com <yes505304@ipactory.com>
2025-09-25 16:20:37 +0900

    update header part

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,32 @@
-import { mdiAccount } from "@mdi/js";
+import { mdiMenu, mdiAccount, mdiLogin } from "@mdi/js";
 import Icon from "@mdi/react";
+import { useState } from "react";
 
 function Header() {
+  const menuContents = [1, 2, 3, 4, 5, 6];
+  const [isLogin, setIsLogin] = useState(false);
+  const [openLoginModal, setopenLoginModal] = useState(false);
+
+  const onClickLoginModal = () => {
+    setopenLoginModal((prev) => !prev);
+  };
   return (
-    <div className="flex justify-end">
-      <Icon path={mdiAccount} size={2} />
-    </div>
+    <>
+      <div className="flex justify-end p-4">
+        <Icon
+          path={isLogin ? mdiAccount : mdiMenu}
+          size={2}
+          onClick={onClickLoginModal}
+        />
+      </div>
+      {openLoginModal && (
+        <div className="w-60 fixed right-5 ">
+          {menuContents.map((_, index) => (
+            <p className="border-1 mb-px">content{index}</p>
+          ))}
+        </div>
+      )}
+    </>
   );
 }
 
